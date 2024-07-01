e293b66c1af60bdf95cf38cdb02cf58bc5575d4c
unknown <ywang123@lqad.com>
2024-07-01 17:36:50 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+</script>
+
+<template>
+  <main>
+    1111
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+<style scoped lang="scss">
+main {
+  background-color: aqua;
+}
+</style>
