faeb89e18affcc9b8337bb01055ddf8516d14c84
street <1205602819@qq.com>
2024-07-21 21:41:47 +0800

    feat: http请求

--- src/api/hotspot.js
+++ src/api/hotspot.js
@@ -1,3 +1,3 @@
 import { axiosGet, axiosPost } from '@/api/request'
 
-export const getList = (data) => axiosGet('/web/extension/detail/reference/app/list', data)
+export const getList = (data) => axiosGet('/risk_analysis/dependency/analysis/list/trade_type', data)
