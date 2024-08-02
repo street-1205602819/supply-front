a649179639b0e575e0bac5e8f48f1893b2001bce
unknown <ywang123@lqad.com>
2024-08-02 10:26:25 +0800

    style: logo和title部分

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { RouterView } from 'vue-router'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 
 const router = useRouter()
+const route = useRoute()
 const store = useStore()
 const menus = computed(() => store.getters.menus)
 
@@ -15,12 +16,18 @@ const onSelect = e => {
   sideBarActive.value = e
   router.push({ name: menus.value[e].children[0].route })
 }
+console.log('route', route);
+
+const title = computed(() => route.meta.title)
 
 </script>
 
 <template>
   <el-container class="app-main">
     <el-aside class="sidebar">
+      <div class="logo-container">
+        <img src="../assets/img/logo.png" alt="" class="app-logo">
+      </div>
       <el-menu :default-active="sideBarActive" class="sidebar-menu" :collapse="isCollapse" @select="onSelect">
         <el-menu-item v-for="(item, index) in menus" :key="index" :index="String(index)">
           <template #title>{{ item.name }}</template>
@@ -28,7 +35,8 @@ const onSelect = e => {
         <el-link class="ai-button" type="primary" href="https://chatglm.cn/applyAndLogin" target="_blank">AI指导</el-link>
       </el-menu>
     </el-aside>
-    <el-container>
+    <el-container class="app-content">
+      <div class="content-title">{{ title }}</div>
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -45,11 +53,13 @@ const onSelect = e => {
 .sidebar {
   height: 100%;
   max-width: 200px;
+  background-color: #f0f2f8;
+  padding: 8px;
 }
 
 .sidebar-menu {
-  height: 100%;
   position: relative;
+  height: calc(100% - 60px);
 }
 
 .ai-button {
@@ -57,5 +67,33 @@ const onSelect = e => {
   bottom: 20px;
   left: 20px;
   font-size: 18px;
+}
+
+.logo-container {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding: 0 0 10px 10px;
+}
+
+.app-content {
+  flex-direction: column;
+}
+
+.content-title {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  font-weight: 600;
+  color: #374050;
+  font-size: 18px;
+  border-bottom: 1px solid rgb(238, 239, 243);
+  background-color: rgb(249, 249, 251);
+}
+
+.app-logo {
+  height: 40px;
+  width: 40px;
 }
 </style>
